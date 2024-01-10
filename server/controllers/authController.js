5da8577c4ccc501419f31b836dee12282b3fb270
yeeyang <yeeyang.kok@sagaos.com>
2024-01-10 15:49:26 +0800

    change origin url

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -20,7 +20,8 @@ const register = async (req, res) => {
   const role = isFirstAccount ? 'admin' : 'user';
 
   const verificationToken = crypto.randomBytes(40).toString('hex');
-  const origin = 'http://localhost:3000'
+  // const origin = 'http://localhost:3000'
+  const origin = 'https://auth-front-end.onrender.com/'
   const user = await User.create({ name, email, password, role, verificationToken});
 
   await sendVerificationEmail({name:user.name, email:user.email,verificationToken:user.verificationToken, origin})
